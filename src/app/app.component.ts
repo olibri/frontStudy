8c44f963595d0b5a4c6871ab1a77753f93dc74c3
Volodya-27 <linecatdog@gmail.com>
2022-02-09 21:46:39 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,68 +1,17 @@
-  import { HttpClient } from '@angular/common/http';
-  import { Component, OnInit } from '@angular/core';
-  import {Subject, Subscription} from 'rxjs'
-  // import { DataService } from './services/data.service';
-  import {delay} from 'rxjs/operators'
-  export interface toDo
-  {
-    comp: boolean;
-    title: string;
-    id: number ;
-  }
-  @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
-  })
-  export class AppComponent implements OnInit {
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import {Subject, Subscription} from 'rxjs'
+import {delay} from 'rxjs/operators'
+import { AuthService } from './auth.service';
 
-    toDos: toDo[] = []
-    todoTitle =''
-    loading = false;
-    constructor(private http: HttpClient){
-
-  }
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  constructor(public auth:AuthService){}
   ngOnInit(): void {
-  this.fetch()
-
-  }
-  addToDo()
-  {
-    if(!this.todoTitle.trim)
-  {
-    return
-  }
-    const newtodo: toDo={
-    title: this.todoTitle,
-    comp:false,
-      id: 0
   }
 
-  this.http.post<toDo>('https://jsonplaceholder.typicode.com/todos',newtodo)
-      .subscribe(tod=>{
-      console.log(tod)
-      this.toDos.push(tod)
-      this.todoTitle = ''
-    })
-  }
-
-  fetch()
-  {
-    this.loading= true;
-
-    this.http.get<toDo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
-      .pipe(delay(1000))
-      .subscribe(respons=> {
-        this.toDos = respons
-        this.loading = false;
-      })
-  }
-  deleteSome(id:number){
-    this.http.delete('https://jsonplaceholder.typicode.com/todos/${id}')
-    .subscribe(()=>{
-      this.toDos = this.toDos.filter(t=> t.id !== id)
-    })
-  }
- }
-
-
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,34 @@
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './post/post.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Style2Directive } from './directives/style2.directive';
-import { DataServices } from './services/data.services';
-import { CounterComponent } from './counter/counter.component';
-import { SwitchComponent } from './switch/switch.component';
 
-import {HttpClientModule}from "@angular/common/http"
+import {HttpClientModule}from "@angular/common/http";
+import { AboutExtraComponent } from './about-extra/about-extra.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    Style2Directive,
-    CounterComponent,
-    SwitchComponent,
-
+    AboutComponent,
+    HomeComponent,
+    PostComponent,
+    PostsComponent,
+    AboutExtraComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
+
   ],
   providers: [
   ],
